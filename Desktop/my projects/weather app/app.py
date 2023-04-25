6f2062699ce22402461c9cbbcbba1f8904ad8a23
obuyajr <felix37brock@gmail.com>
2023-04-25 14:42:51 +0300

    weather-app

--- Desktop/my projects/weather app/app.py
+++ Desktop/my projects/weather app/app.py
@@ -3,26 +3,51 @@ from configparser import ConfigParser
 import requests
 
 
-url = ('api.openweathermap.org/data/2.5/weather?q={}&appid={}')
-
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
 config_file = 'config.ini'
 config = ConfigParser()
 config.read(config_file)
 api_key = config['api_key']['key']
 
+
 def get_weather(city):
     result = requests.get(url.format(city, api_key))
     if result:
-        print(result.content)
+        json = result.json()
+        # (city, country, temp_celsius, temp_fahrenheit, icon, weather)
+        city = json['name']
+        country = json['sys']['country']
+        temp_kelvin = json['main']['temp']
+        temp_celsius = temp_kelvin - 273.15
+        temp_fahrenheit = (temp_kelvin - 273.15) * 9/5 + 32
+        icon = json['weather'][0]['icon']
+        weather = json['weather'][0]['main']
+        final = (city, country, temp_celsius, temp_fahrenheit, icon, weather)
+        return final
     else:
-        return
-    
-    
+        return None
+
 
 def search():
-    pass
+    city = city_text.get()
+    weather = get_weather(city)
+    if weather:
+        locaction_label.config(text=f"{weather[0]}, {weather[1]}")
+        temperature_label.config(text=f"Temperature: {weather[2]:.1f}°C / {weather[3]:.1f}°F")
+        weather_label.config(text=f"Weather: {weather[5]}")
+        icon_url = f"http://openweathermap.org/img/w/{weather[4]}.png"
+        icon_data = requests.get(icon_url).content
+        with open("icon.png", "wb") as f:
+            f.write(icon_data)
+        image.config[bitmap]='icons/{}.png'.format(weather[4])
+    else:
+        locaction_label.config(text="Invalid city name")
+        temperature_label.config(text="")
+        weather_label.config(text="")
+        image.config(bitmap="")
 
-app  = Tk()
+
+app = Tk()
 app.title("WEATHER-app")
 app.geometry('700x300')
 
@@ -30,23 +55,19 @@ city_text = StringVar()
 city_entry = Entry(app, textvariable=city_text)
 city_entry.pack()
 
-search_btn = Button(app, text='search weather', width=12, command= search)
+search_btn = Button(app, text='Search weather', width=12, command=search)
 search_btn.pack()
 
-locaction_label = Label(app, text='Location', font=('bold',20))
+locaction_label = Label(app, text='', font=('bold', 20))
 locaction_label.pack()
 
 image = Label(app, bitmap='')
 image.pack()
 
-temperature_label = Label(app, text='temperature')
+temperature_label = Label(app, text='')
 temperature_label.pack()
 
-weather_label = Label(app, text='weather')
+weather_label = Label(app, text='')
 weather_label.pack()
-
-
-
-
 
 app.mainloop()
